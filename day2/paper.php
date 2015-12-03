c7ec109c3b349a4921a80d32bc9851047bf47f60
Zumochi <zumpyzum@gmail.com>
2015-12-03 13:56:55 +0100

    Solved day 2, second half.

--- day2/paper.php
+++ day2/paper.php
@@ -9,6 +9,7 @@ $input = file("input.txt");
 
 $paper = 0;
 $extra = 0;
+$ribbon = 0;
 foreach($input as $box) {
     // Split dimensions into arrays.
     $dimensions = explode("x", $box);
@@ -25,7 +26,11 @@ foreach($input as $box) {
 
     // Add smallest side as extra paper.
     $extra += ($dimensions[0] * $dimensions[1]);
+
+    $ribbon += (2 * $dimensions[0]) + (2 * $dimensions[1]);
+    $ribbon += ($dimensions[0] * $dimensions[1] * $dimensions[2]);
 }
 
 $total = $paper + $extra;
-echo "The elves need $total square feet of packaging paper.";
+echo "<p>The elves need $total square feet of packaging paper.</p>";
+echo "<p>The elves need a total of $ribbon feet of ribbons to wrap the presents.</p>";
